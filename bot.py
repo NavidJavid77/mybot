b732bdb2e7a91ad026ebf3ddce8d53b66d8d243a
NavidJavid77 <ertebatgostar.sh7@gmail.com>
2025-04-17 12:51:23 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
-from telegram import Bot
+import os
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-TOKEN = 'YOUR-TOKEN' 
+TOKEN = os.getenv("BOT_TOKEN")   
 
 async def start(update, context):
-    await update.message.reply_text("BOT RUNNING..")
+    await update.message.reply_text("BOT RUNNING...")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
-
 app.run_polling()
