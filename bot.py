a0c791bd004efe20684adbbebde32e89dbd0cc51
NavidJavid77 <ertebatgostar.sh7@gmail.com>
2025-04-19 11:04:05 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,6 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from telegram import Bot
+import os
 from telegram.ext import Application
-import asyncio
 
-# اطلاعات ربات
-TOKEN = "BOT_TOKEN"
-GROUP_ID = "CHAT_ID"
+TOKEN = os.getenv("BOT_TOKEN")
 
-async def send_message_to_group(bot: Bot):
-    try:
-        await bot.send_message(chat_id=GROUP_ID, text="پیام خودکار هر 10 ثانیه")
-    except Exception as e:
-        print(f"خطا در ارسال پیام: {e}")
-
-async def start_bot():
-    # ساخت اپلیکیشن
-    app = Application.builder().token(TOKEN).build()
-
-    # ایجاد زمان‌بندی پیام
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(send_message_to_group, "interval", seconds=10, args=[app.bot])
-    scheduler.start()
-
-    print("ربات در حال اجراست...")
-    await app.initialize()
-    await app.start()
-    await app.updater.start_polling()
-    await app.updater.idle()
-
-# فقط اگر مستقیم اجرا شد (نه ایمپورت)، استارت کن
-if __name__ == '__main__':
-    asyncio.run(start_bot())
+application = Application.builder().token(TOKEN).build()
